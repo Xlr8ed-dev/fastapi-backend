e79e404d60b4fe526b9bd7ad2c35bebabdc550ca
Deepak Dabekar <deepak.dabekar@atishay.com>
2024-07-31 21:41:23 +0530

    Added New API

--- main.py
+++ main.py
@@ -68,3 +68,73 @@ async def update_item(item_id: int, item: Item, q: str | None = None):
     if q:
         result.update({"q": q})
     return result
+import os, traceback
+from pydantic import BaseModel
+from typing_extensions import Annotated
+from typing import Union, List, Optional
+from fastapi import FastAPI, Response, status, Query
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+@app.get("/")
+async def greeting(name: Union[str, None] = "Deepak"):
+    return {"greeting": f"Hello {name}"}
+
+
+# Example Of Path Parameter and Query Paramete as Optional
+@app.get("/item/{item_id}")
+async def get_item(item_id: Union[int, None] = 0, skip: Union[bool, None] = False):
+    try:
+        if item_id == 0:
+            return {"id": item_id, "skip": skip, "message": "No Item Exists"}
+        elif item_id > 0 and item_id <= 10:
+            return {"id": item_id, "skip": skip, "message": "Item Found"}
+        elif item_id > 10:
+            return {"id": item_id, "skip": skip, "message": "Item Out Stock"}
+    except Exception as e:
+        print(e, traceback.format_exc())
+        return {"error": "Smomething Went Wrong"}
+
+
+# Example Of Path Parameter
+@app.get("/item/{item_id}/{user_id}")
+async def get_item(item_id: int, user_id: int):
+    try:
+        if item_id == 0:
+            return {"id": item_id, "user_id": user_id}
+        elif item_id > 0 and item_id <= 10:
+            return {"id": item_id, "user_id": user_id}
+        elif item_id > 10:
+            return {"id": item_id, "user_id": user_id}
+    except Exception as e:
+        print(e, traceback.format_exc())
+        return {"error": "Smomething Went Wrong"}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
+from typing import Union
+
+from fastapi import FastAPI, Query
+from typing_extensions import Annotated
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[Union[str, None], Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
